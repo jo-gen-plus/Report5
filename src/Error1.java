3287a267a8b9397d647ca68db43cc17ca1139b38
e165715 <e165715@ie.u-ryukyu.ac.jp>
2016-12-04 16:08:55 +0900

    This program is incomplete.

---
+++ src/Error1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by e165715 on 2016/12/04.
+ *
+ */
+public class Error1 {
+    public static void main(String[] arg){
+        String str = null;
+        System.out.println(str.length());
+
+
+    }
+
+}
