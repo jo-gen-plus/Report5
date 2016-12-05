66bc6e46f9ec862e564db916e3060b927eae86d1
e165715 <e165715@ie.u-ryukyu.ac.jp>
2016-12-05 23:16:52 +0900

    This program is incomplete.

--- src/Bug1.java
+++ src/Bug1.java
@@ -1,11 +1,15 @@
+import java.util.DoubleSummaryStatistics;
+
 /**
  * Created by e165715 on 2016/12/04.
  *
  */
-public class Error1 {
+public class Bug1 {
     public static void main(String[] arg){
         try{
-            String str = null;
+            String str = "3．14";
+            double value ;
+            value = Double.parseDouble(str);
             System.out.println(str.length());
         }catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生しました。");
