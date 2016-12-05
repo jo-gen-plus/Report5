a5c307ed626cc9f260a6247aeeeb8ee45c9d2db7
e165715 <e165715@ie.u-ryukyu.ac.jp>
2016-12-05 23:01:55 +0900

    This program is incomplete.

--- src/Error1.java
+++ src/Error1.java
@@ -4,8 +4,15 @@
  */
 public class Error1 {
     public static void main(String[] arg){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            e.printStackTrace();
+        }
+
+
 
 
     }
